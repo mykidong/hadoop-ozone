2c94774813b635bd334a4beba75a9be317817d9d
Kidong Lee <mykidong@gmail.com>
2020-04-24 16:29:36 +0900

    .

--- hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/kerberos/KerberosTestSkip.java
+++ hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/kerberos/KerberosTestSkip.java
@@ -9,34 +9,28 @@ import java.util.Arrays;
 public class KerberosTestSkip {
 
     @Test
-    public void login() throws Exception
+    public void runCLI() throws Exception
     {
-        try {
-            String princiapl = "mykidong/mc-d02.opasnet.io@OPASNET.IO";
-            String keytab = "/etc/ozone/ozone.keytab";
+        String princiapl = "mykidong/mc-d02.opasnet.io@OPASNET.IO";
+        String keytab = "/etc/ozone/ozone.keytab";
 
-            Configuration conf = new Configuration();
-            conf.set("hadoop.security.authentication", "kerberos");
-            UserGroupInformation.setConfiguration(conf);
+        Configuration conf = new Configuration();
+        conf.set("hadoop.security.authentication", "kerberos");
+        UserGroupInformation.setConfiguration(conf);
 
-            UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(princiapl, keytab);
-            if(ugi == null) {
-                ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(princiapl, keytab);
-                System.out.println("login done with kerberos!");
-            } else {
-                System.out.println("ugi: " + ugi.toString());
+        UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(princiapl, keytab);
+        if(ugi == null) {
+            ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(princiapl, keytab);
+            System.out.println("login done with kerberos!");
+        } else {
+            System.out.println("ugi: " + ugi.toString());
 
-                ugi.checkTGTAndReloginFromKeytab();
-                System.out.println("check tgt done with kerberos!");
-            }
-
-            String[] args = Arrays.asList("getsecret").toArray(new String[0]);
-
-            new org.apache.hadoop.ozone.shell.s3.S3Shell().run(args);
-
-        } catch (Exception e)
-        {
-            e.printStackTrace();
+            ugi.checkTGTAndReloginFromKeytab();
+            System.out.println("check tgt done with kerberos!");
         }
+
+        String[] args = Arrays.asList("getsecret").toArray(new String[0]);
+
+        new org.apache.hadoop.ozone.shell.s3.S3Shell().run(args);
     }
 }
